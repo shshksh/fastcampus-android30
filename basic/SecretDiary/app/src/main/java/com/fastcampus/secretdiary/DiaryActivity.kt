0865be1f46e4d4ab2a2f457988e6e156fa767980
SeongHwan Kim <shshksh58@gmail.com>
2021-07-03 22:32:22 +0900

    핸들러를 사용하여 500ms간 입력이 없을 때 SharedPreference에 데이터를 저장하는 텍스트 변경 리스너 추가

--- basic/SecretDiary/app/src/main/java/com/fastcampus/secretdiary/DiaryActivity.kt
+++ basic/SecretDiary/app/src/main/java/com/fastcampus/secretdiary/DiaryActivity.kt
@@ -1,11 +1,37 @@
 package com.fastcampus.secretdiary
 
+import android.content.Context
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
+import androidx.core.widget.addTextChangedListener
+import com.fastcampus.secretdiary.databinding.ActivityDiaryBinding
 
 class DiaryActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityDiaryBinding
+
+    private val handler = Handler(Looper.getMainLooper())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_diary)
+        binding = ActivityDiaryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val detailPref = getSharedPreferences("diary", Context.MODE_PRIVATE)
+        binding.etDiary.setText(detailPref.getString("detail", ""))
+
+        val runnable = Runnable {
+            detailPref.edit {
+                putString("detail", binding.etDiary.text.toString())
+            }
+        }
+
+        binding.etDiary.addTextChangedListener {
+            handler.removeCallbacks(runnable)
+            handler.postDelayed(runnable, 500)
+        }
     }
 }
