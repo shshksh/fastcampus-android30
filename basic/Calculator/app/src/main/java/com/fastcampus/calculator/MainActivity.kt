f19d9b6838b5edf55f2ed6b16968552c37fec33f
SeongHwan Kim <shshksh58@gmail.com>
2021-07-05 00:36:28 +0900

    basic-calculator
    계산기 키패드 레이아웃 작성

--- basic/Calculator/app/src/main/java/com/fastcampus/calculator/MainActivity.kt
+++ basic/Calculator/app/src/main/java/com/fastcampus/calculator/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.fastcampus.calculator
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun buttonClicked(v: View) {
+
+    }
+    fun resultButtonClicked(v: View) {
+
+    }
+    fun clearButtonClicked(v: View) {
+
+    }
+    fun historyButtonClicked(v: View) {
+
     }
 }
