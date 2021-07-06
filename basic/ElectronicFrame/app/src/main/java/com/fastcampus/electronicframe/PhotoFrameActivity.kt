eab463760fc773359971bfd0c05561d8905352bd
SeongHwan Kim <shshksh58@gmail.com>
2021-07-06 21:47:19 +0900

    Basic - ElectronicFrame
    레이아웃 작성 및 이미지 전환 타이머 구현

--- basic/ElectronicFrame/app/src/main/java/com/fastcampus/electronicframe/PhotoFrameActivity.kt
+++ basic/ElectronicFrame/app/src/main/java/com/fastcampus/electronicframe/PhotoFrameActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.net.toUri
 import com.fastcampus.electronicframe.databinding.ActivityPhotoFrameBinding
+import kotlin.concurrent.timer
 
 class PhotoFrameActivity : AppCompatActivity() {
 
@@ -12,12 +13,15 @@ class PhotoFrameActivity : AppCompatActivity() {
 
     private val photoList = mutableListOf<Uri>()
 
+    private var currentPosition = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPhotoFrameBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         getPhotoUriFromIntent()
+        startTimer()
     }
 
     private fun getPhotoUriFromIntent() {
@@ -26,6 +30,28 @@ class PhotoFrameActivity : AppCompatActivity() {
         for (i in 0 until size) {
             intent.getStringExtra("photo$i")?.let {
                 photoList.add(it.toUri())
+            }
+        }
+    }
+
+    private fun startTimer() {
+        timer(period = 5000) {
+            runOnUiThread {
+                val current = currentPosition
+                val next = (current + 1) % photoList.size
+
+                binding.ivBackgroundPhoto.setImageURI(photoList[current])
+
+                with(binding.ivPhoto) {
+                    alpha = 0f
+                    setImageURI(photoList[next])
+                    animate()
+                        .alpha(1.0f)
+                        .setDuration(1000)
+                        .start()
+                }
+
+                currentPosition = next
             }
         }
     }
