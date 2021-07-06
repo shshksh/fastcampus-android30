f953b8229819332cbeda4dd6baded19c51b53846
SeongHwan Kim <shshksh58@gmail.com>
2021-07-06 20:17:52 +0900

    Basic - ElectronicFrame
    외부 저장소 접근 권한 요청 워크플로우 작성

--- basic/ElectronicFrame/app/src/main/java/com/fastcampus/electronicframe/MainActivity.kt
+++ basic/ElectronicFrame/app/src/main/java/com/fastcampus/electronicframe/MainActivity.kt
@@ -1,12 +1,63 @@
 package com.fastcampus.electronicframe
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.fastcampus.electronicframe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initAddPhotoButton()
+        initStartPhotoFrameModeButton()
+    }
+
+    private fun initAddPhotoButton() {
+        binding.btnAddPhoto.setOnClickListener {
+            when {
+                ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    // 권한이 잘 부여되었을 때 갤러리에서 사진을 선택하는 기능
+                }
+                shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE) -> {
+                    showPermissionContextPopup()
+                }
+                else -> {
+                    requestPermissions(
+                        arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                        1000
+                    )
+                }
+            }
+        }
+    }
+
+    private fun showPermissionContextPopup() {
+        AlertDialog.Builder(this)
+            .setTitle("권한이 필요합니다.")
+            .setMessage("전자액자 앱에서 사진을 불러우기 위해 권한이 필요합니다.")
+            .setPositiveButton("동의하기") { _, _ ->
+                requestPermissions(
+                    arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                    1000
+                )
+            }
+            .setNegativeButton("취소하기") { _, _ -> }
+            .create()
+            .show()
+    }
+
+    private fun initStartPhotoFrameModeButton() {
+        TODO("Not yet implemented")
     }
 }
