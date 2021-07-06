de7471ae19b2ef65a013c3e4207c917f266bea02
SeongHwan Kim <shshksh58@gmail.com>
2021-07-06 21:15:09 +0900

    Basic - ElectronicFrame
    외부 저장소 접근 권한 요청을 Activity Result API를 사용하도록 변경

--- basic/ElectronicFrame/app/src/main/java/com/fastcampus/electronicframe/MainActivity.kt
+++ basic/ElectronicFrame/app/src/main/java/com/fastcampus/electronicframe/MainActivity.kt
@@ -2,6 +2,8 @@ package com.fastcampus.electronicframe
 
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -10,6 +12,15 @@ import com.fastcampus.electronicframe.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val requestReadExternalStorageLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                navigatePhotos()
+            } else {
+                Toast.makeText(this, "권한을 거부하셨습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,19 +38,20 @@ class MainActivity : AppCompatActivity() {
                     this,
                     android.Manifest.permission.READ_EXTERNAL_STORAGE
                 ) == PackageManager.PERMISSION_GRANTED -> {
-                    // 권한이 잘 부여되었을 때 갤러리에서 사진을 선택하는 기능
+                    navigatePhotos()
                 }
                 shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE) -> {
                     showPermissionContextPopup()
                 }
                 else -> {
-                    requestPermissions(
-                        arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
-                        1000
-                    )
+                    requestReadExternalStorageLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
                 }
             }
         }
+    }
+
+    private fun navigatePhotos() {
+
     }
 
     private fun showPermissionContextPopup() {
@@ -47,10 +59,7 @@ class MainActivity : AppCompatActivity() {
             .setTitle("권한이 필요합니다.")
             .setMessage("전자액자 앱에서 사진을 불러우기 위해 권한이 필요합니다.")
             .setPositiveButton("동의하기") { _, _ ->
-                requestPermissions(
-                    arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
-                    1000
-                )
+                requestReadExternalStorageLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
             }
             .setNegativeButton("취소하기") { _, _ -> }
             .create()
@@ -58,6 +67,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initStartPhotoFrameModeButton() {
-        TODO("Not yet implemented")
+//        TODO("Not yet implemented")
     }
 }
