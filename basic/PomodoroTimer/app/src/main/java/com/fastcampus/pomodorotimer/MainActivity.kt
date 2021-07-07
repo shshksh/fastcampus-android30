0431b6af98cc570d8a065cb798bc05f8c88007d0
SeongHwan Kim <shshksh58@gmail.com>
2021-07-07 19:22:32 +0900

    Basic - Pomodoro Timer
    Seekbar 변경 리스너 설정

--- basic/PomodoroTimer/app/src/main/java/com/fastcampus/pomodorotimer/MainActivity.kt
+++ basic/PomodoroTimer/app/src/main/java/com/fastcampus/pomodorotimer/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.fastcampus.pomodorotimer
 
 import android.os.Bundle
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
+import com.fastcampus.pomodorotimer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        bindViews()
+    }
+
+    private fun bindViews() {
+        binding.sbTimer.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                binding.tvRemainMinutes.text = "%02d".format(progress)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+
+        })
     }
 }
