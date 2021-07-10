3bdce1f0f4c9845c00e450614ff2cca9201a7a1c
SeongHwan Kim <shshksh58@gmail.com>
2021-07-10 22:17:24 +0900

    Basic - Recorder
    오디오 관련 권한 설정

--- basic/recorder/app/src/main/java/com/fastcampus/recorder/MainActivity.kt
+++ basic/recorder/app/src/main/java/com/fastcampus/recorder/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.fastcampus.recorder
 
+import android.Manifest
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.fastcampus.recorder.databinding.ActivityMainBinding
 
@@ -10,12 +12,23 @@ class MainActivity : AppCompatActivity() {
 
     private var state = State.BEFORE_RECORDING
 
+    private val audioPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (!isGranted)
+                finish()
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        requestAudioPermission()
         initViews()
+    }
+
+    private fun requestAudioPermission() {
+        audioPermissionLauncher.launch(Manifest.permission.RECORD_AUDIO)
     }
 
     private fun initViews() {
