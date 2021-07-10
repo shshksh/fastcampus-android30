5470058dde25515e60c977013d76935dc1389690
SeongHwan Kim <shshksh58@gmail.com>
2021-07-10 20:02:49 +0900

    Basic - Recorder
    뷰 바인딩 적용과 녹음 버튼 초기화

--- basic/recorder/app/src/main/java/com/fastcampus/recorder/MainActivity.kt
+++ basic/recorder/app/src/main/java/com/fastcampus/recorder/MainActivity.kt
@@ -2,11 +2,23 @@ package com.fastcampus.recorder
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.fastcampus.recorder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
+    private var state = State.BEFORE_RECORDING
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        binding.btnRecord.updateIconWithState(state)
     }
 }
