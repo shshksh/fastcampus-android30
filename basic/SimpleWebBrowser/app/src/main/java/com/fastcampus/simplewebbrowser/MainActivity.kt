2e988417818d1a0910dc2144f59e628424e36281
SeongHwan Kim <shshksh58@gmail.com>
2021-07-11 20:16:49 +0900

    Basic - Simple Web Browser
    웹 뷰 로드 및 주소 입력 동작 구현

--- basic/SimpleWebBrowser/app/src/main/java/com/fastcampus/simplewebbrowser/MainActivity.kt
+++ basic/SimpleWebBrowser/app/src/main/java/com/fastcampus/simplewebbrowser/MainActivity.kt
@@ -1,12 +1,42 @@
 package com.fastcampus.simplewebbrowser
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
+import com.fastcampus.simplewebbrowser.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViews()
+        bindViews()
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun initViews() {
+        binding.webView.apply {
+            webViewClient = WebViewClient()
+            settings.javaScriptEnabled = true
+            loadUrl("http://www.google.com")
+        }
+    }
+
+    private fun bindViews() {
+        binding.apply {
+            etAddressBar.setOnEditorActionListener { v, actionId, _ ->
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    webView.loadUrl(v.text.toString())
+                }
+                false
+            }
+        }
     }
 }
