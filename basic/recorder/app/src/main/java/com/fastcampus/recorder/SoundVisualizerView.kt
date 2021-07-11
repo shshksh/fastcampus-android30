741c4019ee79e36d75a8f56c1eeaa889d252d5f3
SeongHwan Kim <shshksh58@gmail.com>
2021-07-11 16:19:51 +0900

    Basic - Recorder
    녹음되는 음성을 시각화하는 커스텀 뷰 작성

---
+++ basic/recorder/app/src/main/java/com/fastcampus/recorder/SoundVisualizerView.kt
@@ -0,0 +1,89 @@
+package com.fastcampus.recorder
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+
+class SoundVisualizerView(context: Context, attrs: AttributeSet? = null) : View(context, attrs) {
+
+    var onRequestCurrentAmplitude: (() -> Int)? = null
+
+    private val amplitudePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = context.getColor(R.color.purple_500)
+        strokeWidth = LINE_WIDTH
+        strokeCap = Paint.Cap.ROUND
+    }
+    private var drawingWidth = 0
+    private var drawingHeight = 0
+    private var drawingAmplitudes: List<Int> = emptyList()
+    private var isReplaying = false
+    private var replayingPosition = 0
+
+    private val visualizeRepeatAction: Runnable = object : Runnable {
+        override fun run() {
+            if (!isReplaying) {
+                val currentAmplitudes = onRequestCurrentAmplitude?.invoke() ?: 0
+                drawingAmplitudes = listOf(currentAmplitudes) + drawingAmplitudes
+            } else {
+                replayingPosition++
+            }
+
+            invalidate()
+
+            handler?.postDelayed(this, ACTION_INTERVAL)
+        }
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        drawingWidth = w
+        drawingHeight = h
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        canvas ?: return
+
+        val centerY = drawingHeight / 2f
+        var offsetX = drawingWidth.toFloat()
+
+        drawingAmplitudes.let { amplitudes ->
+            if (isReplaying)
+                amplitudes.takeLast(replayingPosition)
+            else
+                amplitudes
+        }.forEach { amplitude ->
+            val lineLength = amplitude / MAX_AMPLITUDE * drawingHeight * 0.8f
+
+            offsetX -= LINE_SPACE
+            if (offsetX < 0) return@forEach
+
+            canvas.drawLine(
+                offsetX, centerY - lineLength / 2,
+                offsetX, centerY + lineLength / 2,
+                amplitudePaint
+            )
+        }
+    }
+
+    fun startVisualizing(isReplaying: Boolean) {
+        this.isReplaying = isReplaying
+        handler?.post(visualizeRepeatAction)
+    }
+
+    fun stopVisualizing() {
+        handler?.removeCallbacks(visualizeRepeatAction)
+    }
+
+    companion object {
+
+        private const val LINE_WIDTH = 10f
+        private const val LINE_SPACE = 15f
+        private const val MAX_AMPLITUDE = Short.MAX_VALUE.toFloat()
+        private const val ACTION_INTERVAL = 20L
+    }
+}
