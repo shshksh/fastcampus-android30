7b8b8b60a7ba53a89b1174774e14b16880fa8d58
SeongHwan Kim <shshksh58@gmail.com>
2021-07-12 16:23:35 +0900

    Intermediate - PushNotificationReceiver
    Firebase 클라우드 메시징 설정 및 테스트 알림 메시지 작성

--- intermediate/PushNotificationReceiver/app/src/main/java/com/fastcampus/pushnotificationreceiver/MainActivity.kt
+++ intermediate/PushNotificationReceiver/app/src/main/java/com/fastcampus/pushnotificationreceiver/MainActivity.kt
@@ -3,6 +3,7 @@ package com.fastcampus.pushnotificationreceiver
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.fastcampus.pushnotificationreceiver.databinding.ActivityMainBinding
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +13,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initFirebase()
+    }
+
+    private fun initFirebase() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                binding.tvFirebaseToken.text = task.result
+            }
+        }
     }
 }
