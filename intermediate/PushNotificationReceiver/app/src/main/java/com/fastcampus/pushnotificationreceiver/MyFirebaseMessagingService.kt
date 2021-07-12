8c27ecc551f0c5e49b45155f9b2645ebc254ca8a
SeongHwan Kim <shshksh58@gmail.com>
2021-07-12 17:16:10 +0900

    Intermediate - PushNotificationReceiver
    FirebaseMessagingService 연동

---
+++ intermediate/PushNotificationReceiver/app/src/main/java/com/fastcampus/pushnotificationreceiver/MyFirebaseMessagingService.kt
@@ -0,0 +1,15 @@
+package com.fastcampus.pushnotificationreceiver
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onNewToken(p0: String) {
+        super.onNewToken(p0)
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+    }
+}
