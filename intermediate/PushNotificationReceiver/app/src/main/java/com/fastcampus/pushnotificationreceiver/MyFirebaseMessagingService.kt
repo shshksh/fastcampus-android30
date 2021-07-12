14ad71625dfb57ce2b7b3fddcc6fb522c6e7b568
SeongHwan Kim <shshksh58@gmail.com>
2021-07-12 17:33:20 +0900

    Intermediate - PushNotificationReceiver
    알림 빌더 생성 및 notify

--- intermediate/PushNotificationReceiver/app/src/main/java/com/fastcampus/pushnotificationreceiver/MyFirebaseMessagingService.kt
+++ intermediate/PushNotificationReceiver/app/src/main/java/com/fastcampus/pushnotificationreceiver/MyFirebaseMessagingService.kt
@@ -1,8 +1,11 @@
 package com.fastcampus.pushnotificationreceiver
 
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -12,11 +15,32 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         super.onNewToken(p0)
     }
 
-    override fun onMessageReceived(message: RemoteMessage) {
-        super.onMessageReceived(message)
+    override fun onMessageReceived(removeMessage: RemoteMessage) {
+        super.onMessageReceived(removeMessage)
 
         createNotificationChannel()
+
+        val title = removeMessage.data["title"]
+        val message = removeMessage.data["message"]
+
+        val notificationBuilder = createNotificationBuilder(title, message)
+
+        notifyMessage(1, notificationBuilder.build())
     }
+
+    private fun notifyMessage(id: Int, notification: Notification) {
+        NotificationManagerCompat.from(this)
+            .notify(id, notification)
+    }
+
+    private fun createNotificationBuilder(
+        title: String?,
+        message: String?
+    ) = NotificationCompat.Builder(this, CHANNEL_ID)
+        .setSmallIcon(R.drawable.ic_baseline_notifications_24)
+        .setContentTitle(title)
+        .setContentText(message)
+        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
 
     private fun createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
