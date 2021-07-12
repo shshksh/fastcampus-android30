c263e2fecb3d760eeaf0bd0287b31a63da0af277
SeongHwan Kim <shshksh58@gmail.com>
2021-07-12 17:25:58 +0900

    Intermediate - PushNotificationReceiver
    알림 채널 생성

--- intermediate/PushNotificationReceiver/app/src/main/java/com/fastcampus/pushnotificationreceiver/MyFirebaseMessagingService.kt
+++ intermediate/PushNotificationReceiver/app/src/main/java/com/fastcampus/pushnotificationreceiver/MyFirebaseMessagingService.kt
@@ -1,5 +1,8 @@
 package com.fastcampus.pushnotificationreceiver
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -11,5 +14,28 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
+
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            channel.description = CHANNEL_DESCRIPTION
+
+            (getSystemService(NOTIFICATION_SERVICE) as NotificationManager)
+                .createNotificationChannel(channel)
+        }
+    }
+
+    companion object {
+
+        private const val CHANNEL_NAME = "Emoji Party"
+        private const val CHANNEL_DESCRIPTION = "Emoji Party를 위한 채널"
+        private const val CHANNEL_ID = "Channel ID"
     }
 }
