fbc649e356628f3f1a5d88f02429d71f48e01ebf
SeongHwan Kim <shshksh58@gmail.com>
2021-07-13 15:17:52 +0900

    Intermediate - Thought of the Day
    remote config 데이터 fetch, json 파싱

--- intermediate/ThoughtOfTheDay/app/src/main/java/com/fastcampus/thoughtoftheday/MainActivity.kt
+++ intermediate/ThoughtOfTheDay/app/src/main/java/com/fastcampus/thoughtoftheday/MainActivity.kt
@@ -3,6 +3,11 @@ package com.fastcampus.thoughtoftheday
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.fastcampus.thoughtoftheday.databinding.ActivityMainBinding
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+import org.json.JSONArray
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,10 +18,34 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initViews()
+        initData()
     }
 
-    private fun initViews() {
-        binding.viewPager.adapter = QuotesPagerAdapter(emptyList())
+    private fun initData() {
+        val remoteConfig = Firebase.remoteConfig
+        remoteConfig.setConfigSettingsAsync(
+            remoteConfigSettings {
+                minimumFetchIntervalInSeconds = 0
+            }
+        )
+        remoteConfig.fetchAndActivate().addOnCompleteListener {
+            if (it.isSuccessful) {
+                val quoteList = parseQuoteListJson(remoteConfig.getString("quote_list"))
+                val isNameRevealed = remoteConfig.getBoolean("is_name_revealed")
+
+                displayQuoteListPager(quoteList, isNameRevealed)
+            }
+        }
+    }
+
+    private fun displayQuoteListPager(quoteList: List<Quote>, isNameRevealed: Boolean) {
+        binding.viewPager.adapter = QuoteListPagerAdapter(quoteList, isNameRevealed)
+    }
+
+    private fun parseQuoteListJson(json: String): List<Quote> {
+        val jsonArray = JSONArray(json)
+
+        return List<JSONObject>(jsonArray.length()) { jsonArray.getJSONObject(it) }
+            .map { Quote(it.getString("quote"), it.getString("name")) }
     }
 }
---
+++ intermediate/ThoughtOfTheDay/app/src/main/java/com/fastcampus/thoughtoftheday/QuoteListPagerAdapter.kt
@@ -0,0 +1,41 @@
+package com.fastcampus.thoughtoftheday
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.fastcampus.thoughtoftheday.databinding.ItemQuoteBinding
+
+class QuoteListPagerAdapter(
+    private val quoteList: List<Quote>,
+    private val isNameRevealed: Boolean
+) : RecyclerView.Adapter<QuoteListPagerAdapter.QuoteViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuoteViewHolder {
+        val quoteBinding =
+            ItemQuoteBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return QuoteViewHolder(quoteBinding)
+    }
+
+    override fun onBindViewHolder(holder: QuoteViewHolder, position: Int) {
+        holder.bind(quoteList[position], isNameRevealed)
+    }
+
+    override fun getItemCount(): Int = quoteList.size
+
+    class QuoteViewHolder(private val binding: ItemQuoteBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(quote: Quote, isNameRevealed: Boolean) {
+            with(binding) {
+                tvQuote.text = quote.quote
+                if (isNameRevealed) {
+                    tvName.text = quote.name
+                    tvName.visibility = View.VISIBLE
+                } else {
+                    tvName.visibility = View.GONE
+                }
+            }
+        }
+    }
+}
