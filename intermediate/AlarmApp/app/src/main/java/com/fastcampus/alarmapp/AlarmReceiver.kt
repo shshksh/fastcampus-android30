a4c2e8c173d9772ee9ea6c67c82f38014c615d1e
SeongHwan Kim <shshksh58@gmail.com>
2021-07-14 17:15:06 +0900

    Intermediate - AlarmApp
    알람 리시버 정의

---
+++ intermediate/AlarmApp/app/src/main/java/com/fastcampus/alarmapp/AlarmReceiver.kt
@@ -0,0 +1,48 @@
+package com.fastcampus.alarmapp
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
+class AlarmReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        createNotificationChannel(context)
+
+        notifyNotification(context)
+    }
+
+    private fun createNotificationChannel(context: Context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                NOTIFICATION_CHANNEL_ID,
+                "기상 알람",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+
+            NotificationManagerCompat.from(context).createNotificationChannel(notificationChannel)
+        }
+    }
+
+    private fun notifyNotification(context: Context) {
+        with(NotificationManagerCompat.from(context)) {
+            val builder = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
+                .setContentTitle("알람")
+                .setContentText("일어날 시가입니다.")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+
+            notify(NOTIFICATION_ID, builder.build())
+        }
+    }
+
+    companion object {
+
+        private const val NOTIFICATION_CHANNEL_ID = "1000"
+        private const val NOTIFICATION_ID = 100
+    }
+}
