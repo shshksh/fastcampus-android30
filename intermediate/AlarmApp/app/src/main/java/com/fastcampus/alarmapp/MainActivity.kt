9c28feb98174769594613bbfc4b4603b323e3734
SeongHwan Kim <shshksh58@gmail.com>
2021-07-14 16:56:24 +0900

    Intermediate - AlarmApp
    알람 데이터를 SharedPreference 에 저장하는 함수 추가

--- intermediate/AlarmApp/app/src/main/java/com/fastcampus/alarmapp/MainActivity.kt
+++ intermediate/AlarmApp/app/src/main/java/com/fastcampus/alarmapp/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.fastcampus.alarmapp
 
+import android.app.TimePickerDialog
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.fastcampus.alarmapp.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +15,50 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initChangeAlarmTimeButton()
+    }
+
+    private fun initChangeAlarmTimeButton() {
+        binding.btnChangeAlarmTimeButton.setOnClickListener {
+            val calendar = Calendar.getInstance()
+
+            TimePickerDialog(
+                this,
+                { view, hourOfDay, minute ->
+                    val model = saveAlarmModel(hourOfDay, minute, false)
+                },
+                calendar.get(Calendar.HOUR_OF_DAY),
+                calendar.get(Calendar.MINUTE),
+                false
+            ).show()
+        }
+    }
+
+    private fun saveAlarmModel(
+        hourOfDay: Int,
+        minute: Int,
+        onOff: Boolean
+    ): AlarmDisplayModel {
+        val model = AlarmDisplayModel(hour = hourOfDay, minute = minute, onOff = false)
+
+        val pref = getSharedPreferences(SHARED_PREFERENCE_TIME, Context.MODE_PRIVATE)
+
+        with(pref.edit()) {
+            putString(ALARM_KEY, model.makeDataForDB())
+            putBoolean(ONOFF_KEY, model.onOff)
+            commit()
+        }
+
+        return model
+    }
+
+    companion object {
+
+        private const val SHARED_PREFERENCE_TIME = "time"
+        private const val ALARM_KEY = "alarm"
+        private const val ONOFF_KEY = "ofOff"
+
+        private const val ALARM_REQUEST_CODE = 1000
     }
 }
