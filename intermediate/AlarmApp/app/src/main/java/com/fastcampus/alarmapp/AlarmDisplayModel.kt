e6d0b02456770e8170bffb41d67fe1a5215da885
SeongHwan Kim <shshksh58@gmail.com>
2021-07-14 16:25:09 +0900

    Intermediate - AlarmApp
    데이터 모델 정의

---
+++ intermediate/AlarmApp/app/src/main/java/com/fastcampus/alarmapp/AlarmDisplayModel.kt
@@ -0,0 +1,25 @@
+package com.fastcampus.alarmapp
+
+data class AlarmDisplayModel(
+    val hour: Int,
+    val minute: Int,
+    var onOff: Boolean
+) {
+
+    val timeText: String
+        get() {
+            val h = "02d".format(if (hour < 12) hour else hour - 12)
+            val m = "02d".format(minute)
+
+            return "$h:$m"
+        }
+
+    val amPmText: String
+        get() {
+            return if (hour < 12) "AM" else "PM"
+        }
+
+    fun makeDataForDB(): String {
+        return "$hour $minute"
+    }
+}
