eeb6b55015985db35ad02e07d71e7f7ef159d7ef
SeongHwan Kim <shshksh58@gmail.com>
2021-07-14 14:43:08 +0900

    Intermediate - AlarmApp
    프로젝트 생성

---
+++ intermediate/AlarmApp/app/src/main/java/com/fastcampus/alarmapp/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.fastcampus.alarmapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.fastcampus.alarmapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
