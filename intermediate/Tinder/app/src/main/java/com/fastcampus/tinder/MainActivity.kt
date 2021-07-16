b1240e9f5aa2cde2e25cf23ed4b4b965fdb62c4c
SeongHwan Kim <shshksh58@gmail.com>
2021-07-16 21:03:51 +0900

    Intermediate - Tinder
    앱 시작 시 로그인 처리

--- intermediate/Tinder/app/src/main/java/com/fastcampus/tinder/MainActivity.kt
+++ intermediate/Tinder/app/src/main/java/com/fastcampus/tinder/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.fastcampus.tinder
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private val auth: FirebaseAuth by lazy { Firebase.auth }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if (auth.currentUser == null) {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }
