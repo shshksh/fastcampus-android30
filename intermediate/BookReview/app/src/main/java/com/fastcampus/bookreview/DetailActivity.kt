ccbb2769ae93b5c5530cbdca7366c3a11a990d01
SeongHwan Kim <shshksh58@gmail.com>
2021-07-16 12:39:35 +0900

    Intermediate - Book Review
    전달받은 데이터를 뷰에 적용

--- intermediate/BookReview/app/src/main/java/com/fastcampus/bookreview/DetailActivity.kt
+++ intermediate/BookReview/app/src/main/java/com/fastcampus/bookreview/DetailActivity.kt
@@ -3,7 +3,9 @@ package com.fastcampus.bookreview
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.room.Room
+import com.bumptech.glide.Glide
 import com.fastcampus.bookreview.databinding.ActivityDetailBinding
+import com.fastcampus.bookreview.model.Book
 
 class DetailActivity : AppCompatActivity() {
 
@@ -21,5 +23,14 @@ class DetailActivity : AppCompatActivity() {
             AppDatabase::class.java,
             "BookSearchDB"
         ).build()
+
+        val model = intent.getParcelableExtra<Book>("bookModel")
+
+        binding.tvTitle.text = model?.title.orEmpty()
+        binding.tvDescription.text = model?.description.orEmpty()
+
+        Glide.with(binding.ivCover.context)
+            .load(model?.coverSmallUrl.orEmpty())
+            .into(binding.ivCover)
     }
 }
