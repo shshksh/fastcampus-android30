530cc68bb1f6a5bfa53b9b9892b270fa7986199a
SeongHwan Kim <shshksh58@gmail.com>
2021-07-16 13:48:17 +0900

    Intermediate - Book Review
    리뷰 엔티티 적용

--- intermediate/BookReview/app/src/main/java/com/fastcampus/bookreview/AppDatabase.kt
+++ intermediate/BookReview/app/src/main/java/com/fastcampus/bookreview/AppDatabase.kt
@@ -3,9 +3,13 @@ package com.fastcampus.bookreview
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.fastcampus.bookreview.dao.HistoryDao
+import com.fastcampus.bookreview.dao.ReviewDao
 import com.fastcampus.bookreview.model.History
+import com.fastcampus.bookreview.model.Review
 
-@Database(entities = [History::class], version = 1)
+@Database(entities = [History::class, Review::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun historyDao(): HistoryDao
+
+    abstract fun reviewDao(): ReviewDao
 }
