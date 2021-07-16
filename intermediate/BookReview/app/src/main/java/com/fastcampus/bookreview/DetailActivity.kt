01c7e89b94709f876a96ae20100c11f861e429e9
SeongHwan Kim <shshksh58@gmail.com>
2021-07-16 13:48:32 +0900

    Intermediate - Book Review
    리뷰 조회 및 저장 기능 추가

--- intermediate/BookReview/app/src/main/java/com/fastcampus/bookreview/DetailActivity.kt
+++ intermediate/BookReview/app/src/main/java/com/fastcampus/bookreview/DetailActivity.kt
@@ -6,6 +6,7 @@ import androidx.room.Room
 import com.bumptech.glide.Glide
 import com.fastcampus.bookreview.databinding.ActivityDetailBinding
 import com.fastcampus.bookreview.model.Book
+import com.fastcampus.bookreview.model.Review
 
 class DetailActivity : AppCompatActivity() {
 
@@ -32,5 +33,20 @@ class DetailActivity : AppCompatActivity() {
         Glide.with(binding.ivCover.context)
             .load(model?.coverSmallUrl.orEmpty())
             .into(binding.ivCover)
+
+        Thread {
+            val review = db.reviewDao().getOneReview(model?.id?.toInt() ?: 0)
+            runOnUiThread {
+                binding.etReview.setText(review?.review.orEmpty())
+            }
+        }.start()
+
+        binding.btnSave.setOnClickListener {
+            Thread {
+                db.reviewDao().saveReview(
+                    Review(model?.id?.toInt() ?: 0, binding.etReview.text.toString())
+                )
+            }.start()
+        }
     }
 }
