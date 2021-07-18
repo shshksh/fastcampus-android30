6c244d8f67ea16a7dea85b46f1d692d950f04cc4
Seonghwan Kim <shshksh58@gmail.com>
2021-07-18 22:09:12 +0900

    Intermediate - Tinder
    showToast 메서드 추출

--- intermediate/Tinder/app/src/main/java/com/fastcampus/tinder/LikeActivity.kt
+++ intermediate/Tinder/app/src/main/java/com/fastcampus/tinder/LikeActivity.kt
@@ -3,14 +3,32 @@ package com.fastcampus.tinder
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.fastcampus.tinder.databinding.ActivityLikeBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class LikeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLikeBinding
 
+    private val auth by lazy { FirebaseAuth.getInstance() }
+
+    private val userDB by lazy { Firebase.database.reference.child("Users") }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLikeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val currentUserDB = userDB.child(getCurrentUserID())
+    }
+
+    private fun getCurrentUserID(): String {
+        if (auth.currentUser == null) {
+            showToast("로그인이 되어있지 않습니다.")
+            finish()
+        }
+
+        return auth.currentUser?.uid.orEmpty()
     }
 }
---
+++ intermediate/Tinder/app/src/main/java/com/fastcampus/tinder/Util.kt
@@ -0,0 +1,12 @@
+package com.fastcampus.tinder
+
+import android.content.Context
+import android.widget.Toast
+
+fun Context.showToast(message: String) {
+    Toast.makeText(
+        this,
+        message,
+        Toast.LENGTH_SHORT
+    ).show()
+}
