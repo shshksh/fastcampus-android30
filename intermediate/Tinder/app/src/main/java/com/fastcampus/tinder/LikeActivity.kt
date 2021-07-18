bd402f64b9634e257fcc7218c8d1c7094d83c403
Seonghwan Kim <shshksh58@gmail.com>
2021-07-18 22:54:14 +0900

    Intermediate - Tinder
    로그인 시 사용자 이름이 없다면 다이얼로그에서 이름을 입력받고 DB 에 저장

--- intermediate/Tinder/app/src/main/java/com/fastcampus/tinder/LikeActivity.kt
+++ intermediate/Tinder/app/src/main/java/com/fastcampus/tinder/LikeActivity.kt
@@ -1,9 +1,14 @@
 package com.fastcampus.tinder
 
 import android.os.Bundle
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.fastcampus.tinder.databinding.ActivityLikeBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
@@ -21,6 +26,44 @@ class LikeActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val currentUserDB = userDB.child(getCurrentUserID())
+        currentUserDB.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.child("name").value == null) {
+                    showNameInputDialog()
+                    return
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+
+    }
+
+    private fun showNameInputDialog() {
+        val editText = EditText(this)
+
+        AlertDialog.Builder(this)
+            .setTitle("이름을 입력해주세요")
+            .setView(editText)
+            .setPositiveButton("저장") { _, _ ->
+                if (editText.text.isEmpty())
+                    showNameInputDialog()
+                else
+                    saveUserName(editText.text.toString())
+            }
+            .setCancelable(false)
+            .show()
+    }
+
+    private fun saveUserName(name: String) {
+        val userId = getCurrentUserID()
+        val currentUserDB = userDB.child(userId)
+        val user = mutableMapOf<String, Any>()
+        user["userId"] = userId
+        user["name"] = name
+        currentUserDB.updateChildren(user)
     }
 
     private fun getCurrentUserID(): String {
