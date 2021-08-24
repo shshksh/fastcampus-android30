d06321d33cb08b84d2a284ad2c553f6dec634315
Seonghwan Kim <shshksh58@gmail.com>
2021-08-25 00:53:41 +0900

    Intermediate - Tinder 매치된 유저를 보여주는 액티비티 추가

---
+++ intermediate/Tinder/app/src/main/java/com/fastcampus/tinder/MatchedUserActivity.kt
@@ -0,0 +1,11 @@
+package com.fastcampus.tinder
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MatchedUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_matched_user)
+    }
+}
