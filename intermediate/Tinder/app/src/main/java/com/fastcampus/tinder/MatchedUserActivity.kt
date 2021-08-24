62e3372ced57ca76435fbd7053615a38d0a7209f
Seonghwan Kim <shshksh58@gmail.com>
2021-08-25 01:02:35 +0900

    Intermediate - Tinder MatchedUserActivity 초기화

--- intermediate/Tinder/app/src/main/java/com/fastcampus/tinder/MatchedUserActivity.kt
+++ intermediate/Tinder/app/src/main/java/com/fastcampus/tinder/MatchedUserActivity.kt
@@ -2,10 +2,43 @@ package com.fastcampus.tinder
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.fastcampus.tinder.databinding.ActivityMatchedUserBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MatchedUserActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMatchedUserBinding
+
+    private val auth by lazy { FirebaseAuth.getInstance() }
+
+    private val userDB by lazy { Firebase.database.reference.child("Users") }
+
+    private val adapter = MatchedUserAdapter()
+    private val cardItems = mutableListOf<CardItem>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_matched_user)
+        binding = ActivityMatchedUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initMatchedUserRecyclerView()
     }
+
+    private fun initMatchedUserRecyclerView() {
+        binding.rvMatchedUser.adapter = adapter
+        binding.rvMatchedUser.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun getCurrentUserID(): String {
+        if (auth.currentUser == null) {
+            showToast("로그인이 되어있지 않습니다.")
+            finish()
+        }
+
+        return auth.currentUser?.uid.orEmpty()
+    }
+
 }
