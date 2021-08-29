f639db1a93f137412e638374bb2679b120548529
Seonghwan Kim <shshksh58@gmail.com>
2021-08-29 17:50:10 +0900

    Chapter6 feat: ArticleAdapter 추가

---
+++ intermediate/chapter6/app/src/main/java/com/fastcampus/chapter6/home/ArticleAdapter.kt
@@ -0,0 +1,52 @@
+package com.fastcampus.chapter6.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.fastcampus.chapter6.databinding.ItemArticleBinding
+import java.text.SimpleDateFormat
+import java.util.*
+
+class ArticleAdapter : ListAdapter<ArticleModel, ArticleAdapter.ViewHolder>(diffUtil) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        ViewHolder(ItemArticleBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(currentList[position])
+    }
+
+    inner class ViewHolder(
+        private val binding: ItemArticleBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(articleModel: ArticleModel) {
+            val df = SimpleDateFormat("MM월 dd일", Locale.KOREA)
+            val date = Date(articleModel.createdAt)
+
+            with(binding) {
+                tvTitle.text = articleModel.title
+                tvDate.text = df.format(date)
+                tvPrice.text = articleModel.price
+
+                if (articleModel.imageUrl.isNotEmpty()) {
+                    Glide.with(ivThumbnail)
+                        .load(articleModel.imageUrl)
+                        .into(ivThumbnail)
+                }
+            }
+        }
+    }
+
+    companion object {
+        private val diffUtil = object : DiffUtil.ItemCallback<ArticleModel>() {
+            override fun areItemsTheSame(oldItem: ArticleModel, newItem: ArticleModel) =
+                oldItem.createdAt == newItem.createdAt
+
+            override fun areContentsTheSame(oldItem: ArticleModel, newItem: ArticleModel) =
+                oldItem == newItem
+        }
+    }
+}
