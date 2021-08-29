45e70b84ac88a8d40c21e8dab6d62c635569ff89
Seonghwan Kim <shshksh58@gmail.com>
2021-08-29 10:51:31 +0900

    Chapter6 feat: 하단 탐색 리스너 설정

--- intermediate/chapter6/app/src/main/java/com/fastcampus/chapter6/MainActivity.kt
+++ intermediate/chapter6/app/src/main/java/com/fastcampus/chapter6/MainActivity.kt
@@ -2,10 +2,44 @@ package com.fastcampus.chapter6
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.fastcampus.chapter6.chatlist.ChatListFragment
+import com.fastcampus.chapter6.databinding.ActivityMainBinding
+import com.fastcampus.chapter6.home.HomeFragment
+import com.fastcampus.chapter6.mypage.MyPageFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        binding.navBottom.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_home -> {
+                    replaceFragment(HomeFragment::class.java)
+                }
+                R.id.menu_chatList -> {
+                    replaceFragment(ChatListFragment::class.java)
+                }
+                R.id.menu_myPage -> {
+                    replaceFragment(MyPageFragment::class.java)
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fClass: Class<out Fragment>) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fClass, null)
+            .commit()
     }
 }
