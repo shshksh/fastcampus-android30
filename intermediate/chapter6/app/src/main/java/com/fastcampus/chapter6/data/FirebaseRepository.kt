637b420e3168f7fca1e848ce2c568409f7156d43
Seonghwan Kim <shshksh58@gmail.com>
2021-08-29 21:25:10 +0900

    Chapter6 feat: 유저 id를 반환하는 함수 구현

--- intermediate/chapter6/app/src/main/java/com/fastcampus/chapter6/data/FirebaseRepository.kt
+++ intermediate/chapter6/app/src/main/java/com/fastcampus/chapter6/data/FirebaseRepository.kt
@@ -1,10 +1,16 @@
 package com.fastcampus.chapter6.data
 
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 object FirebaseRepository {
+    private val auth by lazy { Firebase.auth }
     val articleDB by lazy { Firebase.database.reference.child(DB_ARTICLES) }
+
+    fun isLogin() = auth.currentUser != null
+
+    fun currentUserId() = auth.currentUser?.uid.orEmpty()
 
     private const val DB_ARTICLES = "Articles"
 }
