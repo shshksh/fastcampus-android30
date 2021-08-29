5c32453594946615dd07dc6acc0d2133b50c8fee
Seonghwan Kim <shshksh58@gmail.com>
2021-08-29 17:50:32 +0900

    Chapter6 feat: HomeFragment 뷰 초기화

--- intermediate/chapter6/app/src/main/java/com/fastcampus/chapter6/home/HomeFragment.kt
+++ intermediate/chapter6/app/src/main/java/com/fastcampus/chapter6/home/HomeFragment.kt
@@ -1,7 +1,37 @@
 package com.fastcampus.chapter6.home
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.fastcampus.chapter6.R
+import com.fastcampus.chapter6.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var adapter: ArticleAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentHomeBinding.bind(view)
+        adapter = ArticleAdapter()
+
+        initViews()
+    }
+
+    private fun initViews() {
+        initArticleRecyclerView()
+    }
+
+    private fun initArticleRecyclerView() {
+        binding.rvArticle.layoutManager = LinearLayoutManager(context)
+        binding.rvArticle.adapter = adapter
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
