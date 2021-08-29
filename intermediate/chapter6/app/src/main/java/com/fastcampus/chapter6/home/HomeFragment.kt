ae4d3f26929b179b5d50677060986d8c4202db4d
Seonghwan Kim <shshksh58@gmail.com>
2021-08-29 19:18:29 +0900

    Chapter6 feat: article 리스너 설정

--- intermediate/chapter6/app/src/main/java/com/fastcampus/chapter6/home/HomeFragment.kt
+++ intermediate/chapter6/app/src/main/java/com/fastcampus/chapter6/home/HomeFragment.kt
@@ -5,7 +5,11 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.fastcampus.chapter6.R
+import com.fastcampus.chapter6.data.FirebaseRepository.articleDB
 import com.fastcampus.chapter6.databinding.FragmentHomeBinding
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
     private var _binding: FragmentHomeBinding? = null
@@ -13,12 +17,31 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
 
     private lateinit var adapter: ArticleAdapter
 
+    private val articleList = mutableListOf<ArticleModel>()
+    private val listener = object : ChildEventListener {
+        override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+            val articleModel = snapshot.getValue(ArticleModel::class.java) ?: return
+
+            articleList.add(articleModel)
+            adapter.submitList(articleList)
+        }
+
+        override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {}
+
+        override fun onChildRemoved(snapshot: DataSnapshot) {}
+
+        override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
+
+        override fun onCancelled(error: DatabaseError) {}
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentHomeBinding.bind(view)
         adapter = ArticleAdapter()
 
         initViews()
+        articleDB.addChildEventListener(listener)
     }
 
     private fun initViews() {
@@ -33,5 +56,6 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+        articleDB.removeEventListener(listener)
     }
 }
