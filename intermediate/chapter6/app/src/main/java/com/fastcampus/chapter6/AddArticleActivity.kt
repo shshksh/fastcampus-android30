53264b9336cc5b669f4baad183a5d2c1f67b4c54
Seonghwan Kim <shshksh58@gmail.com>
2021-08-31 00:47:19 +0900

    Chapter6 feat: 사진 업로드 로직 구현

--- intermediate/chapter6/app/src/main/java/com/fastcampus/chapter6/AddArticleActivity.kt
+++ intermediate/chapter6/app/src/main/java/com/fastcampus/chapter6/AddArticleActivity.kt
@@ -1,10 +1,13 @@
 package com.fastcampus.chapter6
 
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import com.fastcampus.chapter6.data.FirebaseRepository.articleDB
+import com.fastcampus.chapter6.data.FirebaseRepository.articleStorage
 import com.fastcampus.chapter6.data.FirebaseRepository.currentUserId
 import com.fastcampus.chapter6.databinding.ActivityAddArticleBinding
 import com.fastcampus.chapter6.home.ArticleModel
@@ -13,12 +16,15 @@ class AddArticleActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAddArticleBinding
 
+    private var selectedUri: Uri? = null
+
     private val getPhotoLauncher =
         registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
             if (uri == null) {
                 Toast.makeText(this, "사진을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
                 return@registerForActivityResult
             }
+            selectedUri = uri
             binding.ivPhoto.setImageURI(uri)
         }
 
@@ -43,14 +49,59 @@ class AddArticleActivity : AppCompatActivity() {
 
     private fun initSubmitButton() {
         binding.btnSubmit.setOnClickListener {
+            showProgress()
+
             val title = binding.etTitle.text.toString()
             val price = binding.etPrice.text.toString()
             val sellerId = currentUserId()
 
-            val model = ArticleModel(sellerId, title, System.currentTimeMillis(), "$price 원", "")
-            articleDB.push().setValue(model)
-
-            finish()
+            selectedUri?.let {
+                uploadPhoto(
+                    it,
+                    { uri -> uploadArticle(sellerId, title, price, uri) },
+                    {
+                        Toast.makeText(this, "사진 업로드에 실패했습니다.", Toast.LENGTH_SHORT).show()
+                        hideProgress()
+                    }
+                )
+            } ?: run {
+                uploadArticle(sellerId, title, price, "")
+            }
         }
+    }
+
+    private fun uploadPhoto(uri: Uri, successHandler: (String) -> Unit, errorHandler: () -> Unit) {
+        val fileName = "${System.currentTimeMillis()}.png"
+        articleStorage.child(fileName)
+            .putFile(uri)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    articleStorage.child(fileName)
+                        .downloadUrl
+                        .addOnSuccessListener { uri ->
+                            successHandler(uri.toString())
+                        }.addOnFailureListener {
+                            errorHandler()
+                        }
+                } else {
+                    errorHandler()
+                }
+            }
+    }
+
+    private fun uploadArticle(sellerId: String, title: String, price: String, imageUrl: String) {
+        val model = ArticleModel(sellerId, title, System.currentTimeMillis(), "$price 원", imageUrl)
+        articleDB.push().setValue(model)
+
+        hideProgress()
+        finish()
+    }
+
+    private fun showProgress() {
+        binding.progressBar.isVisible = true
+    }
+
+    private fun hideProgress() {
+        binding.progressBar.isVisible = false
     }
 }
