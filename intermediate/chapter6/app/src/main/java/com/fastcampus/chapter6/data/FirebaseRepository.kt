381356c7ddf9beb879a163a1a15572fb874ace6e
Seonghwan Kim <shshksh58@gmail.com>
2021-08-31 00:46:36 +0900

    Chapter6 feat: Firebase Storage 프로퍼티 추가

--- intermediate/chapter6/app/src/main/java/com/fastcampus/chapter6/data/FirebaseRepository.kt
+++ intermediate/chapter6/app/src/main/java/com/fastcampus/chapter6/data/FirebaseRepository.kt
@@ -3,10 +3,15 @@ package com.fastcampus.chapter6.data
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 
 object FirebaseRepository {
     private val auth by lazy { Firebase.auth }
+
     val articleDB by lazy { Firebase.database.reference.child(DB_ARTICLES) }
+
+    private val storage by lazy { Firebase.storage }
+    val articleStorage by lazy { storage.reference.child("article/photo") }
 
     fun isLogin() = auth.currentUser != null
 
