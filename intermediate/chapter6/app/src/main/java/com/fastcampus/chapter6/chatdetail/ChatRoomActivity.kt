b6052a22ef97a6e939721f7c6a0b2df624a53f8a
Seonghwan Kim <shshksh58@gmail.com>
2021-09-23 18:38:58 +0900

    Chapter6 feat: ChatRoomActivity 추가

---
+++ intermediate/chapter6/app/src/main/java/com/fastcampus/chapter6/chatdetail/ChatRoomActivity.kt
@@ -0,0 +1,12 @@
+package com.fastcampus.chapter6.chatdetail
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.fastcampus.chapter6.R
+
+class ChatRoomActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat_room)
+    }
+}
