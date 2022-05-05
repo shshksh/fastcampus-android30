836911923e54dd20777d0e7777c17a06dd461351
shshksh <shshksh58@gmail.com>
2022-05-05 12:04:06 +0900

    Chapter07 ui: Implement house list

--- intermediate/chapter07airbnb/app/src/main/java/com/fastcampus/chapter07_airbnb/ui/main/MainScreen.kt
+++ intermediate/chapter07airbnb/app/src/main/java/com/fastcampus/chapter07_airbnb/ui/main/MainScreen.kt
@@ -6,16 +6,19 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.BottomSheetScaffold
 import androidx.compose.material.Card
+import androidx.compose.material.Divider
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -26,7 +29,9 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -49,30 +54,15 @@ import com.naver.maps.map.MapView
 @ExperimentalPagerApi
 @ExperimentalMaterialApi
 @Composable
-fun MainScreen(mapView: MapView) {
+fun MainScreen(mapView: MapView, viewModel: MainViewModel = viewModel()) {
     val scaffoldState = rememberBottomSheetScaffoldState()
+    val houseList by viewModel.houseList.collectAsState()
 
     BottomSheetScaffold(
         sheetContent = {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(100.dp),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(
-                    text = "여러개의 숙소",
-                    style = TextStyle(fontSize = 15.sp, fontWeight = FontWeight.Bold)
-                )
-            }
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(color = Color.Cyan),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-
-            }
+            HouseListHeader()
+            Divider(color = Color(0xFFCCCCCC), thickness = 1.dp)
+            HouseList(houseList)
         },
         scaffoldState = scaffoldState,
         sheetPeekHeight = 100.dp,
@@ -83,7 +73,7 @@ fun MainScreen(mapView: MapView) {
     ) {
         Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.BottomCenter) {
             NaverMapView(mapView = mapView)
-            HousePager(modifier = Modifier.padding(bottom = 120.dp))
+            HousePager(modifier = Modifier.padding(bottom = 120.dp), houseList)
         }
     }
 }
@@ -119,9 +109,7 @@ private fun getMapViewLifecycleObserver(mapView: MapView) = LifecycleEventObserv
 
 @ExperimentalPagerApi
 @Composable
-private fun HousePager(modifier: Modifier = Modifier, viewModel: MainViewModel = viewModel()) {
-    val houseList by viewModel.houseList.collectAsState()
-
+private fun HousePager(modifier: Modifier = Modifier, houseList: List<HouseModel>) {
     HorizontalPager(
         count = houseList.size,
         modifier = modifier
@@ -174,6 +162,78 @@ fun HouseCard(houseModel: HouseModel) {
     }
 }
 
+@Composable
+private fun HouseListHeader() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(100.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 12.dp),
+            contentAlignment = Alignment.TopCenter
+        ) {
+            Box(
+                modifier = Modifier
+                    .width(30.dp)
+                    .height(3.dp)
+                    .background(
+                        color = Color(0xffcccccc),
+                        shape = RoundedCornerShape(15.dp)
+                    )
+            )
+        }
+        Text(
+            text = "여러개의 숙소",
+            style = TextStyle(fontSize = 15.sp, fontWeight = FontWeight.Bold)
+        )
+    }
+}
+
+@Composable
+fun HouseList(houseList: List<HouseModel>) {
+    LazyColumn {
+        items(count = houseList.size) { index ->
+            HouseListItem(houseList[index])
+        }
+    }
+}
+
+@Composable
+private fun HouseListItem(houseModel: HouseModel) {
+    Column(modifier = Modifier.padding(24.dp)) {
+        AsyncImage(
+            model = houseModel.imgUrl,
+            contentDescription = null,
+            modifier = Modifier
+                .aspectRatio(3f / 2f)
+                .fillMaxSize()
+                .clip(RoundedCornerShape(12.dp)),
+            contentScale = ContentScale.Crop
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        Text(
+            text = houseModel.title,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight(),
+            fontSize = 20.sp
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        Text(
+            text = houseModel.price,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight(),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
 @Preview(showBackground = true, widthDp = 300, heightDp = 100)
 @Composable
 fun HouseCardPreview() {
@@ -188,5 +248,22 @@ fun HouseCardPreview() {
 
     Chapter07airbnbTheme {
         HouseCard(sampleModel)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HouseListItemPreview() {
+    val sampleModel = HouseModel(
+        id = 0,
+        title = "미금역 오피스텔",
+        price = "5000/80",
+        imgUrl = "",
+        lat = 37.0,
+        lng = 127.0
+    )
+
+    Chapter07airbnbTheme {
+        HouseListItem(sampleModel)
     }
 }
