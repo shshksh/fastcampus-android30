9ac7856140447ecd9ea03a7b33b96c4b2f870273
shshksh <shshksh58@gmail.com>
2022-06-19 21:04:50 +0900

    Part4-Ch01 feat: Link transition progress between activity and fragment

--- upper-intermediate/ch1youtube/app/src/main/java/com/fastcampus/ch1youtube/MainActivity.kt
+++ upper-intermediate/ch1youtube/app/src/main/java/com/fastcampus/ch1youtube/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.fastcampus.ch1youtube
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.fastcampus.ch1youtube.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun updateMotionProgress(progress: Float) {
+        binding.motionLayout.progress = progress
     }
 }
--- upper-intermediate/ch1youtube/app/src/main/java/com/fastcampus/ch1youtube/PlayerFragment.kt
+++ upper-intermediate/ch1youtube/app/src/main/java/com/fastcampus/ch1youtube/PlayerFragment.kt
@@ -1,6 +1,35 @@
 package com.fastcampus.ch1youtube
 
+import android.os.Bundle
+import android.view.View
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.constraintlayout.motion.widget.TransitionAdapter
 import androidx.fragment.app.Fragment
+import com.fastcampus.ch1youtube.databinding.FragmentPlayerBinding
 
 class PlayerFragment : Fragment(R.layout.fragment_player) {
+
+    private var _binding: FragmentPlayerBinding? = null
+    private val binding: FragmentPlayerBinding
+        get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        _binding = FragmentPlayerBinding.bind(view)
+
+        binding.motionLayout.setTransitionListener(object : TransitionAdapter() {
+            override fun onTransitionChange(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int,
+                progress: Float,
+            ) {
+                (requireActivity() as? MainActivity)?.updateMotionProgress(progress)
+            }
+        })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
