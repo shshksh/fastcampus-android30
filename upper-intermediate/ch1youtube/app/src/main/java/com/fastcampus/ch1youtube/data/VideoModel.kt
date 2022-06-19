cb7491c5c79a20e2f41f1fe834d4a2f9f05ec137
shshksh <shshksh58@gmail.com>
2022-06-19 21:59:10 +0900

    Part4-Ch01 feat: Implement video data model

---
+++ upper-intermediate/ch1youtube/app/src/main/java/com/fastcampus/ch1youtube/data/VideoModel.kt
@@ -0,0 +1,9 @@
+package com.fastcampus.ch1youtube.data
+
+data class VideoModel(
+    val description: String,
+    val sources: String,
+    val subtitle: String,
+    val thumb: String,
+    val title: String,
+)
---
+++ upper-intermediate/ch1youtube/app/src/main/java/com/fastcampus/ch1youtube/network/VideoListResponse.kt
@@ -0,0 +1,16 @@
+package com.fastcampus.ch1youtube.network
+
+import com.squareup.moshi.Json
+
+
+data class VideoListResponse(
+    @field:Json(name = "videos") val videos: List<VideoResponse>,
+)
+
+data class VideoResponse(
+    @field:Json(name = "description") val description: String,
+    @field:Json(name = "sources") val sources: String,
+    @field:Json(name = "subtitle") val subtitle: String,
+    @field:Json(name = "thumb") val thumb: String,
+    @field:Json(name = "title") val title: String,
+)
