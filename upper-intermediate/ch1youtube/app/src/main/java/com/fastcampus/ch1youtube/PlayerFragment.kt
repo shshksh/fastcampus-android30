db17c741c8d0eec028ad83320780f355d6bef64f
shshksh <shshksh58@gmail.com>
2022-07-03 12:31:51 +0900

    Part4-Ch01 feat: Implement recycler view in player fragment

--- upper-intermediate/ch1youtube/app/src/main/java/com/fastcampus/ch1youtube/PlayerFragment.kt
+++ upper-intermediate/ch1youtube/app/src/main/java/com/fastcampus/ch1youtube/PlayerFragment.kt
@@ -5,7 +5,14 @@ import android.view.View
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.constraintlayout.motion.widget.TransitionAdapter
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.fastcampus.ch1youtube.adapter.VideoAdapter
 import com.fastcampus.ch1youtube.databinding.FragmentPlayerBinding
+import kotlinx.coroutines.launch
 
 class PlayerFragment : Fragment(R.layout.fragment_player) {
 
@@ -13,9 +20,19 @@ class PlayerFragment : Fragment(R.layout.fragment_player) {
     private val binding: FragmentPlayerBinding
         get() = _binding!!
 
+    private val viewModel by viewModels<MainViewModel>()
+
+    private lateinit var videoAdapter: VideoAdapter
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         _binding = FragmentPlayerBinding.bind(view)
 
+        initMotionLayoutEvent()
+        initRecyclerView()
+        observeVideoList()
+    }
+
+    private fun initMotionLayoutEvent() {
         binding.motionLayout.setTransitionListener(object : TransitionAdapter() {
             override fun onTransitionChange(
                 motionLayout: MotionLayout?,
@@ -26,6 +43,24 @@ class PlayerFragment : Fragment(R.layout.fragment_player) {
                 (requireActivity() as? MainActivity)?.updateMotionProgress(progress)
             }
         })
+    }
+
+    private fun initRecyclerView() {
+        videoAdapter = VideoAdapter()
+        binding.rvAdditionalVideo.adapter = videoAdapter
+        binding.rvAdditionalVideo.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    private fun observeVideoList() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch {
+                    viewModel.videoList.collect {
+                        videoAdapter.submitList(it)
+                    }
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
