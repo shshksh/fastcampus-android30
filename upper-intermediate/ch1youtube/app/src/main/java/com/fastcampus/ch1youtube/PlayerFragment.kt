829d27bab40735255ca0c749ce9fbd40a4a2b249
shshksh <shshksh58@gmail.com>
2022-07-03 18:28:12 +0900

    Part4-Ch01 feat: Implement video playing using exo player

--- upper-intermediate/ch1youtube/app/src/main/java/com/fastcampus/ch1youtube/PlayerFragment.kt
+++ upper-intermediate/ch1youtube/app/src/main/java/com/fastcampus/ch1youtube/PlayerFragment.kt
@@ -1,5 +1,6 @@
 package com.fastcampus.ch1youtube
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.constraintlayout.motion.widget.MotionLayout
@@ -12,6 +13,10 @@ import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.fastcampus.ch1youtube.adapter.VideoAdapter
 import com.fastcampus.ch1youtube.databinding.FragmentPlayerBinding
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DefaultDataSource
 import kotlinx.coroutines.launch
 
 class PlayerFragment : Fragment(R.layout.fragment_player) {
@@ -24,11 +29,14 @@ class PlayerFragment : Fragment(R.layout.fragment_player) {
 
     private lateinit var videoAdapter: VideoAdapter
 
+    private var player: ExoPlayer? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         _binding = FragmentPlayerBinding.bind(view)
 
         initMotionLayoutEvent()
         initRecyclerView()
+        initPlayer()
 
         viewLifecycleOwner.lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
@@ -36,6 +44,18 @@ class PlayerFragment : Fragment(R.layout.fragment_player) {
                 launch { observeVideoClickEvent() }
             }
         }
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        player?.pause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        player?.release()
     }
 
     private fun initMotionLayoutEvent() {
@@ -57,6 +77,12 @@ class PlayerFragment : Fragment(R.layout.fragment_player) {
         binding.rvAdditionalVideo.layoutManager = LinearLayoutManager(requireContext())
     }
 
+    private fun initPlayer() {
+        player = ExoPlayer.Builder(requireContext()).build()
+
+        binding.playerView.player = player
+    }
+
     private suspend fun observeVideoList() {
         viewModel.videoList.collect {
             videoAdapter.submitList(it)
@@ -65,6 +91,14 @@ class PlayerFragment : Fragment(R.layout.fragment_player) {
 
     private suspend fun observeVideoClickEvent() {
         viewModel.videoClickEvent.collect {
+
+            val dataSourceFactory = DefaultDataSource.Factory(requireContext())
+            val mediaSource = ProgressiveMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(MediaItem.fromUri(Uri.parse(it.sources)))
+            player?.setMediaSource(mediaSource)
+            player?.prepare()
+            player?.play()
+
             binding.motionLayout.transitionToEnd()
             binding.tvBottomTitle.text = it.title
         }
