b3203455ea494ed953c421c8b2154c28af60d88e
shshksh <shshksh58@gmail.com>
2022-07-10 14:35:34 +0900

    Part04-Ch02 feat: Implement play pause control

--- upper-intermediate/ch2mellon/app/src/main/java/com/fastcampus/ch2mellon/PlayerFragment.kt
+++ upper-intermediate/ch2mellon/app/src/main/java/com/fastcampus/ch2mellon/PlayerFragment.kt
@@ -8,6 +8,9 @@ import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.ListAdapter
 import com.fastcampus.ch2mellon.databinding.FragmentPlayerBinding
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
 import kotlinx.coroutines.launch
 
 class PlayerFragment : Fragment(R.layout.fragment_player) {
@@ -16,21 +19,63 @@ class PlayerFragment : Fragment(R.layout.fragment_player) {
     private val binding: FragmentPlayerBinding
         get() = _binding!!
 
+    private var player: ExoPlayer? = null
+
     private lateinit var adapter: PlayListAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         _binding = FragmentPlayerBinding.bind(view)
 
+        initPlayerView()
         initPlayListButton()
+        initPlayControlButtons()
         initRecyclerView()
 
         getVideoList()
+    }
+
+    private fun initPlayerView() {
+        player = ExoPlayer.Builder(requireContext()).build()
+
+        binding.playerView.player = player
+
+        player?.addListener(object : Player.Listener {
+            override fun onIsPlayingChanged(isPlaying: Boolean) {
+                super.onIsPlayingChanged(isPlaying)
+
+                if (isPlaying) {
+                    binding.playControlImageView.setImageResource(R.drawable.baseline_pause_48)
+                } else {
+                    binding.playControlImageView.setImageResource(R.drawable.baseline_play_arrow_48)
+                }
+            }
+        })
     }
 
     private fun initPlayListButton() {
         binding.playListImageView.setOnClickListener {
             binding.playerViewGroup.isVisible = !binding.playerViewGroup.isVisible
             binding.playListViewGroup.isVisible = !binding.playListViewGroup.isVisible
+        }
+    }
+
+    private fun initPlayControlButtons() {
+        binding.playControlImageView.setOnClickListener {
+            val player = player ?: return@setOnClickListener
+
+            if (player.isPlaying) {
+                player.pause()
+            } else {
+                player.play()
+            }
+        }
+
+        binding.skipNextImageView.setOnClickListener {
+
+        }
+
+        binding.skipPrevImageView.setOnClickListener {
+
         }
     }
 
@@ -46,8 +91,19 @@ class PlayerFragment : Fragment(R.layout.fragment_player) {
         viewLifecycleOwner.lifecycleScope.launch {
             musicService.listMusics().musics
                 .mapIndexed { index, musicEntity -> musicEntity.mapper(index.toLong()) }
+                .also { setMusicList(it) }
                 .submitTo(adapter)
         }
+    }
+
+    private fun setMusicList(modelList: List<MusicModel>) {
+        player?.addMediaItems(modelList.map {
+            MediaItem.Builder()
+                .setMediaId(it.id.toString())
+                .setUri(it.streamUrl)
+                .build()
+        })
+        player?.prepare()
     }
 
     override fun onDestroyView() {
